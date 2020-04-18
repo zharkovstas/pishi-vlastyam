309c1b4ae2c9e3c454399a1ed4fa044915c13726
Stanislav Zharkov <zharkovstas@gmail.com>
2020-04-18 11:35:15 +0500

    add generation

---
+++ src/generate.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from jinja2 import Environment, PackageLoader, select_autoescape
+import sys
+from distutils.dir_util import copy_tree
+import yaml
+
+
+def main(args):
+    is_release = "release" in args
+
+    sections = load_yaml("./structure.yml")["sections"]
+    authorities = load_yaml("./authorities.yml")["authorities"]
+
+    for section in sections:
+        for column in section["columns"]:
+            for entry in column:
+                entry["authorities"] = [a for a in authorities if set(a["tags"]).issuperset(set(entry["tags"]))]
+
+    env = Environment(
+        loader=PackageLoader('generate', './templates'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('home.html')
+    template.stream(release=is_release, sections=sections).dump('../out/index.html')
+
+    copy_tree("./static", "../out")
+
+
+def load_yaml(path):
+    with open(path, 'r') as stream:
+        return yaml.safe_load(stream)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
