c2b33b68b52108f99a3d528284ecdd496a1fbe13
Stanislav Zharkov <zharkovstas@gmail.com>
2026-01-20 11:10:07 +0400

    feat: github pages

--- src/generate.py
+++ src/generate.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from jinja2 import Environment, PackageLoader, select_autoescape
+import os
+import shutil
+import stat
 import sys
-from distutils.dir_util import copy_tree
 import yaml
 from pathlib import Path
 
@@ -15,25 +17,63 @@ def main(args):
     for section in sections:
         for column in section["columns"]:
             for entry in column:
-                entry["authorities"] = [a for a in authorities if set(
-                    a["tags"]).issuperset(set(entry["tags"]))]
+                entry["authorities"] = [
+                    a
+                    for a in authorities
+                    if set(a["tags"]).issuperset(set(entry["tags"]))
+                ]
 
     env = Environment(
-        loader=PackageLoader('generate', './templates'),
-        autoescape=select_autoescape(['html', 'xml'])
+        loader=PackageLoader("generate", "./templates"),
+        autoescape=select_autoescape(["html", "xml"]),
     )
 
     Path("../out").mkdir(parents=True, exist_ok=True)
-    (env.get_template('home.html')
-     .stream(release=is_release, sections=sections)
-     .dump('../out/index.html'))
+    (
+        env.get_template("home.html")
+        .stream(release=is_release, sections=sections)
+        .dump("../out/index.html")
+    )
 
-    copy_tree("./static", "../out")
+    copytree("./static", "../out")
 
 
 def load_yaml(path):
-    with open(path, 'r') as stream:
+    with open(path, "r") as stream:
         return yaml.safe_load(stream)
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        shutil.copystat(src, dst)
+    lst = os.listdir(src)
+
+    if ignore:
+        exclude = ignore(src, lst)
+        lst = [x for x in lst if x not in exclude]
+
+    for item in lst:
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+
+        if symlinks and os.path.islink(s):
+            if os.path.lexists(d):
+                os.remove(d)
+
+            os.symlink(os.readlink(s), d)
+
+            try:
+                st = os.lstat(s)
+                mode = stat.S_IMODE(st.st_mode)
+                os.lchmod(d, mode)
+            except:
+                pass  # lchmod not available
+
+        elif os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 
 if __name__ == "__main__":
